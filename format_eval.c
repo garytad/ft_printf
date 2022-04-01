a36ee08e2fdc59bb51e18d3da3053e9d48eabdfa
gary <garytad@gmail.com>
2022-04-02 04:23:56 +0800

    Beta Test

--- format_eval.c
+++ format_eval.c
@@ -7,7 +7,7 @@ int	format_eval(const char c, va_list args)
 		return (ft_print_char(args));
 	if (c == 's')
 		return (ft_print_string(args));
-	if (c == 'd')
+	if (c == 'd' || 'i')
 		return (ft_print_decimal(args));
 	if (c == 'u')
 		return (ft_print_uint(args));
