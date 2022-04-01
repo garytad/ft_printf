6d9e22a9b7f6f1f8e5bb257f3381d535a65c3c19
gary <garytad@gmail.com>
2022-04-02 02:38:23 +0800

    Makefile and header added

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,28 @@
 #include "ft_printf.h"
 
+/* check for format conversion and print string */
+static int	ft_vprintf(const char *format, va_list args)
+{
+	int	i;
+	int	ret;
+
+	i = 0;
+	ret = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i])
+				ret += format_eval(format[i], args);
+		}
+		else
+			ret += write(1, &format[i], 1);
+		i++;
+	}
+	return (ret);
+}
+
 /* printf format conversion function */
 int	ft_printf(const char *format, ...)
 {
